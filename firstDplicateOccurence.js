80aebbb76b148e6f26690ee157f65e9c5243bc14
Amulya Kashyap <amulyakashyap09@gmail.com>
2021-04-08 11:28:23 +0530

    new question added

---
+++ firstDplicateOccurence.js
@@ -0,0 +1,19 @@
+function firstDuplicateValue(array) {
+  // Write your code here.
+	let indexMap = {};
+	let index = array.length+1; // this index will not exists
+	// and can be used to check if any elemt found
+	for (let i=0; i<array.length; i++){
+		if (!(array[i] in indexMap)) {
+			indexMap[array[i]] = i;
+		}else {
+			if (index > i) index = i;
+			indexMap[array[i]] = i;
+		}
+	}
+	if (index === array.length+1 || array.length < 1) return -1
+  return array[index];
+}
+
+// Do not edit the line below.
+console.log(firstDuplicateValue([2, 1, 5, 2, 3, 3, 4]));
